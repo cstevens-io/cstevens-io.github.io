3dbb83831bc7b57b05b2d23cb01079be39ea1edd
Chris Stevens <splooge@pwned.com>
2020-06-30 09:47:48 -0700

    add replicaset output

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2020.06.30.01.41',
+    VERSION: '2020.06.30.16.46',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
